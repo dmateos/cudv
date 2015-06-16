2cd45d4d44de5041652d8121037cfbefb1363fdf
Daniel Mateos <daniel@mateos.cc>
2015-06-16 18:26:17 +1000

    d5 client now working pretty much

--- cudv-rails/app/assets/javascripts/application.js
+++ cudv-rails/app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
-//= require d3
 //= require_tree .
--- cudv-rails/app/controllers/pages_controller.rb
+++ cudv-rails/app/controllers/pages_controller.rb
@@ -1,7 +1,4 @@
 class PagesController < ApplicationController
   def index
-    @orders = Order.all
-    @products = Product.all
-    @users = User.all
   end
 end
