ef784be651f7045473d247a32c7838817d616e9f
Daniel Mateos <daniel@mateos.cc>
2015-06-20 01:30:43 +1000

    can now upload orgs

--- scripts-misc/cudv_upload_d7.php
+++ scripts-misc/cudv_upload_d7.php
@@ -7,22 +7,22 @@ drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 $orders = db_query("SELECT order_id FROM uc_orders");
 
 foreach($orders as $o) {
-	if($order->status == "abandoned") {
-		continue;
-	}
+    if($order->status == "abandoned") {
+        continue;
+    }
 
-	$order = uc_order_load($o->order_id);
-	$user = user_load($order->uid);
-	$products = array();
+    $order = uc_order_load($o->order_id);
+    $user = user_load($order->uid);
+    $products = array();
 
-	foreach($order->products as $product) {
-		$product->sell_price = $product->price; //wtffffff
-		cudv_client_d7_create_product_if_none($product);
-		$products[] = $product->nid;
-	}
-	$partner = cudv_client_d7_get_product_partner(reset($order->products));
+    foreach($order->products as $product) {
+        $product->sell_price = $product->price; //wtffffff
+        cudv_client_d7_create_product_if_none($product);
+        $products[] = $product->nid;
+    }
+    $partner = cudv_client_d7_get_product_partner(reset($order->products));
 
-	print_r(cudv_client_d7_create_user_if_none($user));
-	print_r(cudv_api_post_order($order->order_id, $user->uid, $partner, "discount", $order->order_total, $products, $order->created));
+    print_r(cudv_client_d7_create_user_if_none($user));
+    print_r(cudv_api_post_order($order->order_id, $user->uid, $partner, "discount", $order->order_total, $products, $order->created));
 }
 ?>
